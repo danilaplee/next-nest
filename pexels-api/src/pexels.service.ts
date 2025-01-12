b99aaa834c5c5b0f7431cd8f9c34a052f4851f07
Daniel <fiend.of.sound@gmail.com>
2025-01-13 00:18:12 +0100

    feat: add single page

--- pexels-api/src/pexels.service.ts
+++ pexels-api/src/pexels.service.ts
@@ -6,7 +6,7 @@ export class PexelsService {
   constructor() {
     this.pexelsClient = createClient(process.env.PEXELS_API_KEY);
   }
-  async curated(perPage: number = 16, page: number = 1) {
+  async curated(perPage: number = 100, page: number = 1) {
     return this.pexelsClient.photos.curated({ per_page: perPage, page });
   }
   async searchPhotos(query: string, perPage: number = 16, page: number = 1) {
