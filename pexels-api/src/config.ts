6c0fae26741584e51731fdee7594e08aa3bab85e
Daniel <fiend.of.sound@gmail.com>
2025-01-22 18:17:53 +0100

    feat: split microservice deployment

--- pexels-api/src/config.ts
+++ pexels-api/src/config.ts
@@ -3,7 +3,7 @@ export const config = {
     host: (process.env.REDIS_HOST as string) || 'localhost',
     port: !isNaN(parseInt(process.env.REDIS_PORT, 10)) ? parseInt(process.env.REDIS_PORT, 10) : 6379,
     username: process.env.REDIS_USER || undefined,
-    password: process.env.REDIS_PASSWORD || undefined,
+    password: process.env.REDIS_PASS || undefined,
   },
   isQueueConsumer: process.env.QUEUE_CONSUMER || undefined, 
   apiProducer: process.env.API_PRODUCER || undefined, 
