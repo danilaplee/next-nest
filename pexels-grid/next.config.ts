43bc7ef66bd856f1446cbe1a9ea8ef3175d1c373
Daniel <fiend.of.sound@gmail.com>
2025-01-23 00:23:28 +0100

    feat: split microservice deployment

--- pexels-grid/next.config.ts
+++ pexels-grid/next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  publicRuntimeConfig: {
-    PUBLIC_API_URL: process.env.PUBLIC_API_URL,
-  },
-  serverRuntimeConfig:{
-    SSR_API_URL: process.env.SSR_API_URL,
-  }
 };
 
 export default nextConfig;
--- pexels-grid/src/utils/getServerEnv.ts
+++
@@ -1,7 +0,0 @@
-
-'use server';
-import { config } from "@/config";
-export async function getServerEnv() {
-  'use server';
-  return {API_URL:config.API_URL}
-}
