9358edd21aeb0614106fa0ec254a3acf3a761eb9
Daniel <fiend.of.sound@gmail.com>
2025-01-22 19:16:24 +0100

    feat: split microservice deployment

--- pexels-grid/next.config.ts
+++ pexels-grid/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   publicRuntimeConfig: {
     SSR_API_URL: process.env.SSR_API_URL,
-    SECOND_VAR: process.env.PUBLIC_API_URL,
+    PUBLIC_API_URL: process.env.PUBLIC_API_URL,
   }
 };
 
