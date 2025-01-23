d135a0e8a3df14469dc093a8d5960b371e961dfa
Daniel <fiend.of.sound@gmail.com>
2025-01-23 02:36:07 +0100

    feat: add video support

--- pexels-grid/src/components/SearchButton.tsx
+++ pexels-grid/src/components/SearchButton.tsx
@@ -34,6 +34,7 @@ export default function SearchButton({ video }: { video?: boolean }) {
           className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
           onClick={() => {
             router.replace("/videos");
+            router.refresh()
           }}
         >
           Videos
@@ -43,6 +44,7 @@ export default function SearchButton({ video }: { video?: boolean }) {
           className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
           onClick={() => {
             router.replace("/");
+            router.refresh()
           }}
         >
           Images
