056d2d3f5a4377604755d0ee916f8c0063c8c82a
Daniel <fiend.of.sound@gmail.com>
2025-01-25 13:42:31 +0100

    feat: update routing

--- pexels-grid/src/components/Gallery.tsx
+++ pexels-grid/src/components/Gallery.tsx
@@ -26,7 +26,7 @@ export default function Gallery({
         <div style={{ position: "fixed", top: 20, left: 20, zIndex: 10000 }}>
           <Button
             className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
-            onClick={() => router.replace("/")}
+            onClick={() => router.push("/")}
           >
             Home
           </Button>
